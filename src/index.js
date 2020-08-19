a3df9e5c4835131550c58fb46616dec97a62250e
mj305 <maria.j.beckles@gmail.com>
2020-08-19 09:23:45 -0400

    errors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
-import 'dotenv/config';
-import express from'express';
-import mongoose from 'mongoose';
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
-const app = express()
+const app = express();
 const port = process.env.PORT;
 
-mongoose.connect(process.env.DBURL, {useNewUrlParser: true});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.DBURL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("MongoDB Connected!")
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("MongoDB Connected!");
 });
 
+app.get("/", (req, res) => {
+  return res.status(201).json({ name: "Hello World" });
+});
 
-app.get('/', (req, res) => {
-  return res.status(201).json({'name': 'Hello World'})
-})
+app.post("/signup", (req, res) => {
+  const { email, userUID } = req.body;
+  return res.status(201).json({ message: "Success" });
+});
 
 app.listen(port, () => {
-  console.log(`We are live at http://localhost:${port}`)
-})
+  console.log(`We are live at http://localhost:${port}`);
+});
